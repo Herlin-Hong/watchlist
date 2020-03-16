b585ba4aa8774cdc44fc2c8e944d64e7de6c3826
harlin <35719610+Herlin-Hong@users.noreply.github.com>
2020-03-16 15:51:48 +0800

    add index page

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask, escape,url_for
+from flask import Flask, escape,url_for,render_template
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/home')
 @app.route('/')
 def hello():
     # return 'Welcome to My Watchlist!'
     # return u'欢迎 to My Watchlist!'
-    return '<h1>Hello Totoro~~~</h1><img src="http://helloflask.com/totoro.gif">'
-
+    # return '<h1>Hello Totoro~~~</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'user : {}'.format(escape(name))
+
+@app.route('/user_id/<int:id>')
+def user(id):
+    return 'user : {}'.format(escape(id))
 
 @app.route('/test')
 def test_url_for():
